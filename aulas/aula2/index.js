3f9ebbde85fb84a143774b75c5429a024bee3e29
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-01 12:14:18 -0300

    nova01

--- aulas/aula2/index.js
+++ aulas/aula2/index.js
@@ -1,4 +1,10 @@
 let nome = "Thaiz";
-
+/*Apresenta sem problemas o que programamos*/
 console.log(nome);
+/*É uma erro*/
 console.error(nome);
+/*Não é um erro mais devemos prestar atenção*/
+console.warn(nome);
+
+/*Janela web*/
+window.alert(nome);
