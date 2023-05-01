db067a8b0c19cebdeaafe0944c6036d87b6eda5e
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-01 12:33:37 -0300

    nova3

--- aulas/aula2/index.js
+++ aulas/aula2/index.js
@@ -6,5 +6,12 @@ console.error(nome);
 /*Não é um erro mais devemos prestar atenção*/
 console.warn(nome);
 
-/*Janela web*/
-window.alert(nome);
+/*Janela web
+window.alert(nome);*/
+/*
+nome = window.prompt("Insira o seu nome:");
+
+console.log(nome);*/
+let confirmacao = window.confirm("Confirma essa ação?");
+
+console.log(confirmacao);
