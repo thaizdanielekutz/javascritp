ad3e2d89f5fa4325663d04a1200096735a5a0632
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-03 12:32:16 -0300

    nov1

---
+++ aulas/aula4/index.js
@@ -0,0 +1,10 @@
+const idade = prompt("Informe a sua idade:");
+if (idade > 18) {
+  alert("Você é maior de idade");
+} else if (idade > 12) {
+  alert("Você é menor de idade");
+} else if (idade > 4) {
+  alert("Você é criança");
+} else {
+  alert("..");
+}
