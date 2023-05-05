9f218e828ef21fba9568e1f887692630b0c0b786
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-05 12:17:19 -0300

    aula3

---
+++ aulas/aula6/index.js
@@ -0,0 +1,15 @@
+const resultado = prompt("Escolha uma altermativa: \na) \nb) \nc)");
+
+switch ("resultado") {
+  case "a":
+    alert("O resultado é 'a'");
+    break;
+  case "b":
+    alert("O resultado é 'b'");
+    break;
+  case "c":
+    alert("O resultado é 'c'");
+    break;
+  default:
+    alert("Finalizando...");
+}
