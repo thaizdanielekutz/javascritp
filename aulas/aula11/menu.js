250a7172f6f56a51724e28eabc61c2fdef7fdb81
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-09 12:18:19 -0300

    menu

--- aulas/aula11/menu.js
+++ aulas/aula11/menu.js
@@ -10,4 +10,25 @@ do {
       "\n4. Opção Quatro" +
       "\n5. Encerrar"
   );
+
+  switch (opcao) {
+    case "1":
+      alert("Você escolhei a opção 1");
+      break;
+    case "2":
+      alert("Você escolhei a opção 2");
+      break;
+    case "3":
+      alert("Você escolhei a opção 3");
+      break;
+    case "4":
+      alert("Você escolhei a opção 5");
+      break;
+    case "5":
+      alert("Você escolhei a Encerrar");
+      alert("Encerando...");
+      break;
+    default:
+      alert("Opção inválida.");
+  }
 } while (opcao !== "5");
