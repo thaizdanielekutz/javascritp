3a28f6724880b49305c2831cffb4cc9883707b2b
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-09 12:06:38 -0300

    menu interativo

---
+++ aulas/aula11/menu.js
@@ -0,0 +1,13 @@
+let opcao = "5";
+
+do {
+  opcao = prompt(
+    "Seja bem-vindo(a)!\n" +
+      "\nEscolha uma das opções abaixo:" +
+      "\n1. Opção Um" +
+      "\n2. Opção Dois" +
+      "\n3. Opção Três" +
+      "\n4. Opção Quatro" +
+      "\n5. Encerrar"
+  );
+} while (opcao !== "5");
