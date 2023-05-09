7075a0770a4dd597b0af2e2aafc7399e33506aaa
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-09 12:44:12 -0300

    finanças

---
+++ aulas/aula11/financas.js
@@ -0,0 +1,27 @@
+let saldo = prompt("Informe a quantidade de dinheiro inicial:");
+saldo = parseFloat(saldo);
+let opcao = "";
+
+do {
+  opcao = prompt(
+    "Saldo disponível: R$ " +
+      saldo +
+      "\n1. Adicionar dinheiro" +
+      "\n2. Remover dinheiro" +
+      "\n Sair"
+  );
+
+  switch (opcao) {
+    case "1":
+      saldo += parseFloat(prompt("Informe o valor a ser adicionado: "));
+      break;
+    case "2":
+      saldo -= prompt("Informe o valor a ser retirado:");
+      break;
+    case "3":
+      alert("Saindo...");
+      break;
+    default:
+      alert("Entrada Inválida!");
+  }
+} while (opcao !== "3");
