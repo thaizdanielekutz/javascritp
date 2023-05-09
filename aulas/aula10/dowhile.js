0b6273ceb60a8d040f2e50addb2bec0f32233a11
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-09 06:52:29 -0300

    aula do while

---
+++ aulas/aula10/dowhile.js
@@ -0,0 +1,6 @@
+let velocidade = 0;
+do {
+  alert("A velocidade do veículo é " + velocidade + " Km/h");
+  velocidade -= 20;
+} while (velocidade > 0);
+alert("Velocidade final: " + velocidade + " km/h");
