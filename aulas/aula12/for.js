0643c09373914ac72959d654acf03f714ecdc4cb
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-11 12:22:31 -0300

    aula12

---
+++ aulas/aula12/for.js
@@ -0,0 +1,9 @@
+//inicialização; condição; finalização
+let indice = 0;
+for (
+  "Inicializando o for: ";
+  indice <= 10;
+  alert("Finalizando um bloco de repetição")
+) {
+  alert("Indice: " + indice);
+}
