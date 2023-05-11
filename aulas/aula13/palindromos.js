9a07c4af589ce6f115aaf4355d58a78d927671fa
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-11 19:59:15 -0300

    aula13palindromos

---
+++ aulas/aula13/palindromos.js
@@ -0,0 +1,15 @@
+const palavra = prompt("Informe uma palavra: ");
+let palavraInvertida = "";
+
+///marcação
+for (let i = palavra.length - 1; i >= 0; i--) {
+  palavraInvertida += palavra[i];
+}
+
+if (palavra === palavraInvertida) {
+  alert(palavra + " é um palíndromo!");
+} else {
+  alert(
+    palavra + " não é um palíndromo!\n\n" + palavra + " !== " + palavraInvertida
+  );
+}
