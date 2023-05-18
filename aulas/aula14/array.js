81c344fcef9b3e478d0f1b8f3d209b1538493b77
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-18 12:12:37 -0300

    array

---
+++ aulas/aula14/array.js
@@ -0,0 +1,12 @@
+const listaDeCompras = ["Açucar, false"];
+console.log(listaDeCompras);
+
+listaDeCompras[0] = "Arroz";
+listaDeCompras[1] = "Feijão";
+listaDeCompras[2] = "Macarrão";
+listaDeCompras[3] = "Batata";
+listaDeCompras[4] = "Carne";
+
+console.log(listaDeCompras);
+
+console.log(listaDeCompras[4]);
