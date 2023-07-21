a278cbf468d9d58fb57c42b2adfb6c73b6298481
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-07-21 18:12:46 -0300

    html e script inicial de um projeto de calculo de dano

---
+++ projetos/calculo_de_dano/script.js
@@ -0,0 +1,4 @@
+const name = prompt("Insira o nome do personagem: ");
+const poweratach = prompt("Insira o poder de ataque do personagem: ");
+const namePersonage = prompt("Insira o nome do seu personagem: ");
+const quantLife = prompt("Insira a quantidade de pontos de vida: ");
