c9ae21b36afe870376b28acf162d8027ffca89c9
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-07-21 20:35:38 -0300

    calculo de dano

--- projetos/calculo_de_dano/script.js
+++ projetos/calculo_de_dano/script.js
@@ -2,3 +2,13 @@ const name = prompt("Insira o nome do personagem: ");
 const poweratach = prompt("Insira o poder de ataque do personagem: ");
 const namePersonage = prompt("Insira o nome do seu personagem: ");
 const quantLife = prompt("Insira a quantidade de pontos de vida: ");
+const powerDefense = pront("Insira o poder de defesa de outro personagem: ");
+const defense = prompt("Possui escudo: (Sim/Não)");
+
+if((poweratach > powerDefense) && defense === "Não"){
+    alert("O dano é de: " + (poweratach - powerDefense))
+} if else((powerDefense > powerDefense) && defense === "Sim"){
+    alert("O dano é de : " + (1/2(poweratach - powerDefense)))
+} else(poweratach <= powerDefense ){
+    alert("")
+}
