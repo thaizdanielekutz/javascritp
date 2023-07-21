53c4ed27472efa7657105a57174a26eaf04ae671
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-07-21 20:41:05 -0300

    ajuste parsefloat

--- projetos/teste_de_velocidade/script.js
+++ projetos/teste_de_velocidade/script.js
@@ -1,7 +1,9 @@
 const auto1 = prompt("Digíte o nome do primeiro carro:");
-const velocidade1 = prompt("Digíte a velocidade do primeiro carro:");
+const velocidade1 = parseFloat(
+  prompt("Digíte a velocidade do primeiro carro:")
+);
 const auto2 = prompt("Digíte o nome do segundo carro: ");
-const velocidade2 = prompt("Digíte a velocidade do segundo carro:");
+const velocidade2 = parseFloat(prompt("Digíte a velocidade do segundo carro:"));
 
 if (velocidade1 > velocidade2) {
   alert("O " + auto1 + " é mais rápido que o " + auto2);
