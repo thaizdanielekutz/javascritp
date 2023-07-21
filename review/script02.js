6f85593d3f99e76ccec920ae933f5ad312db88d2
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-07-21 16:16:07 -0300

    criação de um script js e a transcrição de um exemplo de javascript e suas condições if e else

---
+++ review/script02.js
@@ -0,0 +1,11 @@
+// Outro exemplo
+
+const idadeN = prompt("Digíte sua idade: ");
+
+if (idadeN > 18) {
+  alert("Você é maior de idade.");
+} else if (idadeN < 12) {
+  alert("Você é menor de idade.");
+} else if (idadeN < 4) {
+  alert("Você é uma criança.");
+}
