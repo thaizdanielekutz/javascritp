d919632c0c9099309f05b70326ae423ae40a20c5
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-07-22 14:40:41 -0300

    estruturando switch

---
+++ review/switch/switch.js
@@ -0,0 +1,15 @@
+const resultado = prompt("Escolha uma alternativa: \na) \nb) \nc) \nd)");
+
+switch ("b") {
+  case "a":
+    alert("O resultado é 'a'. ");
+  case "b":
+    alert("O resultado é 'b'. ");
+    break;
+  case "c":
+    alert("O resultado é 'c'. ");
+  case "d":
+    alert("O resultado é 'd'. ");
+  default:
+    alert("Finalizando...");
+}
