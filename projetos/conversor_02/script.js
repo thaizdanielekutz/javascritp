01f133d062e23d0e4130cc0700f2d8676758ab38
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-07-23 08:51:06 -0300

    construção de um switch com cases de opções para converter medidas

---
+++ projetos/conversor_02/script.js
@@ -0,0 +1,33 @@
+const medida = prompt("Insira uma medida em metros: ");
+
+const unidade = prompt(
+  "Para qual unidade de medida deseja converter?" +
+    "\n1. milímetros (mm)" +
+    "\n2. centímetros (cm)" +
+    "\n3. decímetros (dm)" +
+    "\n4. decâmetros (dam)" +
+    "\n5. hectômetros (hm)" +
+    "\n6. quilômetros (km)"
+);
+
+switch (unidade) {
+  case "1":
+    alert("Resultado: " + medida + "m = " + medida * 1000 + " mm");
+    break;
+  case "2":
+    alert("Resultado: " + medida + "m = " + medida * 100 + " cm");
+    break;
+  case "3":
+    alert("Resultado: " + medida + "m = " + medida * 10 + " dm");
+    break;
+  case "4":
+    alert("Resultado: " + medida + "m = " + medida / 10 + " dam");
+    break;
+  case "5":
+    alert("Resultado: " + medida + "m = " + medida / 100 + " hm");
+    break;
+  case "6":
+    alert("Resultado: " + medida + "m = " + medida / 1000 + " km");
+  default:
+    alert("Opção inválida.");
+}
