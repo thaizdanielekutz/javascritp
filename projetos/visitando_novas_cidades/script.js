6be53b1c7b4c445f8fd1200eb80d553c13c7eb53
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-07-26 12:47:19 -0300

    alert com concatenação de mensagem

--- projetos/visitando_novas_cidades/script.js
+++ projetos/visitando_novas_cidades/script.js
@@ -11,3 +11,12 @@ while (continuar === "Sim") {
   contagem++;
   continuar = prompt("Você visitou alguma outra cidade? (Sim/Não)");
 }
+
+alert(
+  "Turista: " +
+    nameVisit +
+    "\nQuantidade de cidades visitadas: " +
+    contagem +
+    "\nCidades visitadas:\n " +
+    cidades
+);
