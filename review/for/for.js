254283d1d61bc64594e6f618f4e3acdf0ddeaa18
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-07-28 18:39:47 -0300

    ajuste ao codigo for

---
+++ review/for/for.js
@@ -0,0 +1,14 @@
+/*for(let indice = 0; indice <= 10; indice++){
+    alert("Índice = " + indice)
+}*/
+
+let indice = 0;
+
+for (
+  alert("Inicializando o for");
+  indice <= 10;
+  alert("Finalizando um bloco de repetição")
+) {
+  alert("Índice = " + indice);
+  indice++;
+}
