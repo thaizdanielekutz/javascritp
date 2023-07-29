1b9be91c3d6a7714b2e98d859c52dce709eb1f2f
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-07-29 11:41:50 -0300

    Criação de arrays

---
+++ estrutura_de_dados_e_function_for_else/array/script.js
@@ -0,0 +1,10 @@
+const listaDeCompras = [];
+
+listaDeCompras[0] = "Arroz";
+listaDeCompras[1] = "Feijão";
+listaDeCompras[2] = "Vinagre";
+listaDeCompras[3] = "Carne";
+listaDeCompras[4] = "Banana";
+listaDeCompras[5] = "Suco";
+
+console.log(listaDeCompras);
