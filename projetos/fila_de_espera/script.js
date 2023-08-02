cba6c1c845ca6154de138f13fdc141b17296f316
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-02 12:33:11 -0300

    const do projeto inicial

---
+++ projetos/fila_de_espera/script.js
@@ -0,0 +1,16 @@
+const listaPacientes = [
+  "Carlos",
+  "Pedro",
+  "Lucas",
+  "João",
+  "Luiz",
+  "Pedrinho",
+  "Sérgio",
+];
+
+const novopaciente = prompt("1.Novo paciente: ")
+
+switch(){
+    case "1":
+        alert()
+}
