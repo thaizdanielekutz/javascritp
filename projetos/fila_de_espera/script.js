9bdd9499194fab25774c86cc7b60b1c02490a0fb
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-02 12:48:56 -0300

    configuração de menu

--- projetos/fila_de_espera/script.js
+++ projetos/fila_de_espera/script.js
@@ -6,4 +6,9 @@ do {
   for (let i = 0; i < fila.length; i++) {
     paciente += i + 1 + "º - " + fila[i] + "\n";
   }
+  opcao = prompt(
+    "Pacientes: \n" +
+      paciente +
+      "\nEscolha uma opção: \n1. Novo paciente \n2. Consultar paciente \n3. Sair"
+  );
 } while (opcao !== "3");
