fe9bff7fa5a66cb9e5292a849edd9839c11a1a24
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-07 06:40:54 -0300

    construção da função switch

--- projetos/pilha_de_cartas/script.js
+++ projetos/pilha_de_cartas/script.js
@@ -3,4 +3,12 @@ baralho = ""
 
 do{
     opcao = prompt("Cartas no baralho " + baralho.length + "\n1. Adicionar uma carta \n2.Puxar uma carta \n3. Sair")
+
+    switch(opcao){
+        case "1":
+        case "2":
+        case "3":
+            default: 
+            alert("opção inválida")
+    }
 }while(opcao !== "3")
