9e17b2acd8412182aebe1c705d0f4f93ca848b0e
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-14 06:44:34 -0300

    verificando processo de encadeamento de arrays para acessar elementos especíicos

--- javascript_ii/array_bidimensionais/script.js
+++ javascript_ii/array_bidimensionais/script.js
@@ -4,10 +4,15 @@ console.log(arr);
 // Níveis diferentes de array
 const arrNivelado = [
   "1° Nível",
-  ["2° Nível, 42, true"],
+  ["2° Nível", 42, true],
   [
-    ["3° Nível, 1º item", "Olá, mundo!"],
-    ["3º Nível, 2º item", "Oi, mundo!"],
+    ["3° Nível", "1º item", "Olá, mundo!"],
+    ["3º Nível", "2º item", "Oi, mundo!"],
   ],
   [],
 ];
+console.log(arrNivelado);
+
+// Para referenciar array dentro de um array - encadear
+console.log(arrNivelado[1]);
+console.log(arrNivelado[1][0]);
