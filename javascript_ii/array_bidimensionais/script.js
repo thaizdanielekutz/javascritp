9c328983238d6e5ea25cbd3f4ac68bece48991ee
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-14 06:33:05 -0300

    array de diferentes níveis

--- javascript_ii/array_bidimensionais/script.js
+++ javascript_ii/array_bidimensionais/script.js
@@ -1,2 +1,13 @@
 const arr = [["1° Nível", "2° Nível", "3° nível"]];
 console.log(arr);
+
+// Níveis diferentes de array
+const arrNivelado = [
+  "1° Nível",
+  ["2° Nível, 42, true"],
+  [
+    ["3° Nível, 1º item", "Olá, mundo!"],
+    ["3º Nível, 2º item", "Oi, mundo!"],
+  ],
+  [],
+];
