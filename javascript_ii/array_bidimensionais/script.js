3fa3f0ab37dc634ecbbd5c6c34b6a62466372572
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-14 06:55:09 -0300

    criando um array  matriz

--- javascript_ii/array_bidimensionais/script.js
+++ javascript_ii/array_bidimensionais/script.js
@@ -16,3 +16,9 @@ console.log(arrNivelado);
 // Para referenciar array dentro de um array - encadear
 console.log(arrNivelado[1]);
 console.log(arrNivelado[1][0]);
+
+const matriz = [
+  ["l1, c1", "l1, c2", "l1, c3", "l1, c4"][
+    ("l2, c1", "l2, c2", "l2, c3", "l2, c4")
+  ][("l3, c1", "l3, c2", "l3, c3", "l3, c4")],
+];
