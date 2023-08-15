c65e46fa60b14f4358b488bfbd6c299093cfc2cd
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-15 06:55:06 -0300

    Criação de objeto

---
+++ javascript_ii/projetos/imobiliaria/script.js
@@ -0,0 +1,6 @@
+let imovel = {};
+
+imovel.proprietário = prompt("Informe o nome do proprietário do imóvel: ");
+imovel.quartos = prompt("Quantos quartos possui o imóvel: ");
+imovel.baneiros = prompt("Quantos banheiros possui o imóvel: ");
+imovel.garagem = prompt("O imóvel possi garagem: (SIM/NÃO)");
