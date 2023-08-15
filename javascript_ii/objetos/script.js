7615882f9b1cfbd5668ef7067d2e4f2812aec2d1
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-15 06:48:34 -0300

    exemplo

--- javascript_ii/objetos/script.js
+++ javascript_ii/objetos/script.js
@@ -26,3 +26,22 @@ let funcao = "log";
 console.log(funcao);
 
 console[funcao]("Olá, Mundo!");
+
+// Alguns exemplos
+let pessoa = {};
+pessoa.nome = "Thaiz";
+pessoa.idade = 22;
+pessoa.colegas = ["Lucas", "Juliana", "Rafael"];
+pessoa.endereço = { rua: "Logo ali", número: 42, bairro: "Centro" };
+console.log(pessoa);
+
+let matriz = [
+  [0, 1, 2, 3, 4],
+  [5, 6, 7, 8, 9],
+  [10, 11, 12, 13, 14],
+  [15, 16, 17, 18, 19],
+  20,
+];
+
+console.log(matriz[4][1]);
+console.table(matriz);
