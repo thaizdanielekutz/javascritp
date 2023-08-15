39acdeba76d28b84deb34bc8425376c9f382b21b
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-15 12:31:40 -0300

    menu constituido de do e while com 3 opções

--- javascript_ii/projetos/imobiliaria/script.js
+++ javascript_ii/projetos/imobiliaria/script.js
@@ -1,3 +1,15 @@
+const imoveis = [];
+let opcao = "";
+
+do {
+  opcao = prompt(
+    "Bem-Vindo(a) ao Cadastro de Imovéis!\n" +
+      "Total de Imóveis: " +
+      imoveis.length +
+      "\n\n Escolha uma opção:\n1. Novo Imóvel \n2. Listar Imóveis \n3. Sair"
+  );
+} while (opcao !== "3");
+
 let imovel = {};
 
 imovel.proprietário = prompt("Informe o nome do proprietário do imóvel: ");
