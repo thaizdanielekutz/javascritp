bc01022317fff447933dc00eba43b3c51b592548
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-15 05:36:54 -0300

    criação de um objeto

---
+++ javascript_ii/objetos/script.js
@@ -0,0 +1,11 @@
+let nome = "prop";
+console.log(nome);
+
+let objeto = {};
+console.log(objeto);
+
+//criar um propriedade
+objeto.prop = "Olá, mundo!";
+console.log(objeto);
+
+console.log(objeto.prop);
