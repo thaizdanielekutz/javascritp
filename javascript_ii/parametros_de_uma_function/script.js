f652184d01fcef904aad3a9f8ebe20eec93ecaa8
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-21 12:42:54 -0300

    funções e parâmentros

---
+++ javascript_ii/parametros_de_uma_function/script.js
@@ -0,0 +1,32 @@
+function dobro(x) {
+  alert("O dobro de " + x + " é " + x * 2);
+}
+
+//dobro(5);
+
+function dizerOla(nome = "Mundo") {
+  alert("Olá, " + nome + " !");
+}
+
+//dizerOla("Thaiz");
+
+//Para passar mais um parâmetro temos que adicionar mais uma vírgula
+
+function soma(a, b, c) {
+  alert("O resultado da soma é " + (a + b + c));
+}
+
+//soma(7, 6,0);
+//soma(1, 1,3);
+
+//DETALHES
+//quando temos mais de um parâmetro a ordem importa, nesse caso abaixo abreviações aconteceram
+
+function criarUsuario(nome, email, senha, tipo) {
+  const usuario = {
+    nome,
+    email,
+    senha,
+    tipo,
+  };
+}
