e959b54da2cd31467bc5f452577b38295019f92a
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-29 12:23:52 -0300

    function criada

--- javascript_ii/escopo/escopo.js
+++ javascript_ii/escopo/escopo.js
@@ -3,3 +3,14 @@ let pokemon = "Charmander";
 function evoluir() {
   pokemon = "Charmander";
 }
+
+console.log(pokemon);
+evoluir();
+console.log(pokemon);
+
+function criarAnimal() {
+  let animal = "Galo";
+}
+
+criarAnimal();
+console.log(animal);
