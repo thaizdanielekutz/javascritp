f2bd658dba6dc236336f7e36bd9dd92032acc019
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-29 12:35:31 -0300

    function criada para verificação de escopo

--- javascript_ii/escopo/escopo.js
+++ javascript_ii/escopo/escopo.js
@@ -4,13 +4,31 @@ function evoluir() {
   pokemon = "Charmander";
 }
 
-console.log(pokemon);
-evoluir();
-console.log(pokemon);
+//console.log(pokemon);
+//evoluir();
+//console.log(pokemon);
 
 function criarAnimal() {
   let animal = "Galo";
 }
 
-criarAnimal();
-console.log(animal);
+//criarAnimal();
+//console.log(animal);
+
+// Declaração utilizando o var
+
+function avaliarNota(nota) {
+  if (nota > 60) {
+    var aprovado = true;
+    let situacao = "Aprovado!";
+  } else {
+    var aprovado = false;
+    let situacao = "Reprovado!";
+  }
+  console.log(nota);
+  console.log(aprovado);
+  console.log(situacao);
+}
+
+avaliarNota(83);
+avaliarNota(49);
