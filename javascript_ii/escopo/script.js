726dc1f668527297c66a875d5d44f55ba20447dd
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-30 12:12:16 -0300

    diferença de var e let

---
+++ javascript_ii/escopo/script.js
@@ -0,0 +1,20 @@
+//Assim da erro
+
+console.log(nome);
+console.log(sobrenome);
+
+var nome = "Thaiz";
+let sobrenome = "Pontes";
+
+console.log(nome);
+console.log(sobrenome);
+
+//Mas, se fosse só o var daria certo
+console.log(nome);
+//console.log(sobrenome);
+
+var nome = "Thaiz";
+//let sobrenome = "Pontes";
+
+console.log(nome);
+//console.log(sobrenome);
