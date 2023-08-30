4afa28038846e3dd1dccef9f2d9d81573f514895
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-30 19:08:39 -0300

    estruturando do while

---
+++ javascript_ii/projetos/calculadora_geometric/calculadora.js
@@ -0,0 +1,7 @@
+opcao = "";
+
+do {
+  opcao = prompt(
+    "Seja bem vindo a Calculadora Geométrica: \n1.Triângulo \n2.Retângulo \n3.Quadrado \n4.Trapézio \n5.Círculo \n6.Sair"
+  );
+} while (opcao !== "6");
