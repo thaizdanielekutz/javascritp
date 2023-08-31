1682f28ed2db3acb0e41fa64e81cef386f5954a9
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-08-31 05:58:58 -0300

    estruturando case 1

--- javascript_ii/projetos/calculadora_geometric/calculadora.js
+++ javascript_ii/projetos/calculadora_geometric/calculadora.js
@@ -2,6 +2,17 @@ opcao = "";
 
 do {
   opcao = prompt(
-    "Seja bem vindo a Calculadora Geométrica: \n1.Triângulo \n2.Retângulo \n3.Quadrado \n4.Trapézio \n5.Círculo \n6.Sair"
+    "Seja bem vindo a Calculadora Geométrica: \n\n1.Triângulo \n2.Retângulo \n3.Quadrado \n4.Trapézio \n5.Círculo \n6.Sair"
   );
+  switch (opcao) {
+    case "1":
+      numberOne = prompt("Digíte a base do triângulo: ");
+      numberTwo = prompt("Digíte a altura do triângulo: ");
+
+      function areaTriangular(numberOne, numberTwo) {
+        return numberOne * numberTwo;
+      }
+
+      alert("O resultado é " + result);
+  }
 } while (opcao !== "6");
