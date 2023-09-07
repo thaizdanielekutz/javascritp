b1ff93115c0cb1e1e577be4901d9e6069331a4f2
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-09-07 14:09:57 -0300

    função para calcular area do triangulo

---
+++ javascript_ii/projetos/calculadora_geometric/calc.js
@@ -0,0 +1,5 @@
+function calcularAreaTriangulo() {
+  const base = prompt("Informe a base do triângulo: ");
+  const altura = prompt("Informe a altura do triângulo: ");
+  return (base * altura) / 2;
+}
