9c729f8d6b834e9527de6399f3f432295c024aef
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-09-07 14:12:52 -0300

    calculo da area do retangulo

--- javascript_ii/projetos/calculadora_geometric/calc.js
+++ javascript_ii/projetos/calculadora_geometric/calc.js
@@ -3,3 +3,9 @@ function calcularAreaTriangulo() {
   const altura = prompt("Informe a altura do triângulo: ");
   return (base * altura) / 2;
 }
+
+function calcularAreaRetangulo() {
+  const base = prompt("Informe a base do retângulo: ");
+  const altura = prompt("Informe a altura do retângulo: ");
+  return base * altura;
+}
