8c52de8f623c01e380cc7741f4628ea04807ee4e
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-09-12 06:52:29 -0300

    criando um método

--- javascript_ii/metodos/script.js
+++ javascript_ii/metodos/script.js
@@ -1,6 +1,11 @@
+// Métodos são funções atreladas a objetos
 let pessoa = {
   nome: "Thaiz",
   idade: 22,
+  dizerOla() {
+    console.log("Thaiz, diga olá!");
+  },
 };
 
 console.log(pessoa);
+pessoa.dizerOla();
