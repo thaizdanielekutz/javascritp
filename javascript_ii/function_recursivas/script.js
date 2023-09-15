b99514250fb04da3093e44e11e06ff0658d9a103
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-09-15 06:56:34 -0300

    funções recursivas perigosas

--- javascript_ii/function_recursivas/script.js
+++ javascript_ii/function_recursivas/script.js
@@ -10,3 +10,20 @@ function dividir(num) {
 }
 
 dividir(40);
+
+//PERIGOS DAS FUNÇÕES RECURSIVAS
+// primeira -> segunda -> terceira
+
+// função sem fim
+
+function dobrar(num) {
+  console.log(num);
+  dobrar(num * 2);
+}
+
+dobrar(1);
+
+// FUNÇÃO RECURSIVA MATEMÁTICA fatorial de 5 -> !5 === 5*4*3*2*1 === 5*4!
+function fatorial(num) {
+  console.log(num);
+}
