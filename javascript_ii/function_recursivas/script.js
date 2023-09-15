315108fbb997a3aea5ef90d58399d471cf341a8e
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-09-15 06:59:40 -0300

    estruturando função fatorial

--- javascript_ii/function_recursivas/script.js
+++ javascript_ii/function_recursivas/script.js
@@ -25,5 +25,14 @@ dobrar(1);
 
 // FUNÇÃO RECURSIVA MATEMÁTICA fatorial de 5 -> !5 === 5*4*3*2*1 === 5*4!
 function fatorial(num) {
-  console.log(num);
+  console.log("Número: " + num);
+  if (num === 0) {
+    return 1;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    console.log(num + " * !" + (num - 1));
+    return num * fatorial(num - 1);
+  }
 }
+console.log(fatorial(5));
