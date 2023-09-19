b749839692b0f75e1256eb36071d1668eac4c961
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-09-19 19:03:55 -0300

    função somar em conjunto com anonimas

--- javascript_ii/high_order_functions/script.js
+++ javascript_ii/high_order_functions/script.js
@@ -1,5 +1,13 @@
 function calcular(a, b, operacao) {
   console.log("Realizando uma operação.");
-  const resultado = operacao(a, b);
+  const resultado = operacao(a, b); // somar(3,5)
   return resultado;
 }
+
+function somar(x, y) {
+  console.log("Realizando uma soma.");
+  return x + y;
+}
+calcular(3, 5, somar);
+
+// somar() -> apresenta retorno
